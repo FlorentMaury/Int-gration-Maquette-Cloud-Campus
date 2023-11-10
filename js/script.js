ee165df09883ccacc1fdcd05f0c864aa512ee34e
Florent Maury <flo.neo@hotmail.fr>
2023-11-10 11:23:30 +0100

    Classe active sur la Nav au click.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Sélectionnez tous les éléments de navigation
+var navItems = document.querySelectorAll('.flexNav__li');
+
+// Parcourez chaque élément de navigation
+for (var i = 0; i < navItems.length; i++) {
+    // Ajoutez un écouteur d'événements de clic à chaque élément de navigation
+    navItems[i].addEventListener('click', function(e) {
+        // Empêchez le comportement par défaut du lien
+        e.preventDefault();
+
+        // Supprimez la classe active de tous les éléments de navigation
+        for (var j = 0; j < navItems.length; j++) {
+            navItems[j].classList.remove('flexNav__li--active');
+        }
+
+        // Ajoutez la classe active à l'élément de navigation cliqué
+        this.classList.add('flexNav__li--active');
+    });
+}
